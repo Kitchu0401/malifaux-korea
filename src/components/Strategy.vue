70fccf151ee05a98f582dc66e1ed4e51c6b4c028
Kitchu0401 <kitchu@naver.com>
2021-03-20 02:13:50 +0900

    주차 별 스킴 선택 옵션 추가, 주차 일정 수정

--- src/components/Strategy.vue
+++ src/components/Strategy.vue
@@ -1,87 +1,110 @@
 <template>
-  <div class="accordion" role="tablist">
+  <div>
 
-    <!-- Strategy -->
-    <b-card no-body class="mb-1">
-      <b-card-header header-tag="header" class="p-1" role="tab">
-        <b-button block v-b-toggle.accordion-1 variant="primary">Strategy</b-button>
-      </b-card-header>
-      <b-collapse id="accordion-1" visible accordion="my-accordion" role="tabpanel">
-        <b-card-body>
-          <b-card-text>
-            
-            <b-list-group>
-              <b-list-group-item 
-                v-bind:key="s['symbol']"
-                v-for="s in strategy"
-                class="flex-column align-items-start">
-                <div class="d-flex w-100 justify-content-between">
-                  <h3 class="mb-2">{{ s['title'] }}</h3>
-                  <b-icon font-scale="1.5" icon="eye-slash" v-on:click="hideSymbol(s['symbol'])" />
-                </div>
-                <small><div class="text-left" v-html="s['content']"></div></small>
-              </b-list-group-item>
-            </b-list-group>
-            
-          </b-card-text>
-        </b-card-body>
-      </b-collapse>
-    </b-card>
+    <b-select
+      v-model="selectedRuleset"
+      v-bind:options="selectedRulesetOptions" />
 
-    <!-- Scheme -->
-    <b-card no-body class="mb-1">
-      <b-card-header header-tag="header" class="p-1" role="tab">
-        <b-button block v-b-toggle.accordion-2 variant="primary">Scheme</b-button>
-      </b-card-header>
-      <b-collapse id="accordion-2" visible accordion="my-accordion" role="tabpanel">
-        <b-card-body>
-          <b-card-text>
-            
-            <b-list-group>
-              <b-list-group-item 
-                v-bind:key="s['symbol']"
-                v-for="s in scheme"
-                class="flex-column align-items-start">
-                <div class="d-flex w-100 justify-content-between">
-                  <h3 class="mb-2">{{ s['title'] }}</h3>
-                  <b-icon font-scale="1.5" icon="eye-slash" v-on:click="hideSymbol(s['symbol'])" />
-                </div>
-                <small><div class="text-left" v-html="s['content']"></div></small>
-              </b-list-group-item>
-            </b-list-group>
-            
-          </b-card-text>
-        </b-card-body>
-      </b-collapse>
-    </b-card>
+    <div class="accordion" role="tablist">
 
-    <!-- Reset Button -->
-    <b-card no-body class="mb-1" v-if="hiddenSymbol.length > 0">
-      <b-card-header header-tag="header" class="p-1" role="tab">
-        <b-button block variant="secondary" v-on:click="resetHiddenSymbol">Reset hidden</b-button>
-      </b-card-header>
-    </b-card>
+      <!-- Strategy -->
+      <b-card no-body class="mb-1">
+        <b-card-header header-tag="header" class="p-1" role="tab">
+          <b-button block v-b-toggle.accordion-1 variant="primary">Strategy</b-button>
+        </b-card-header>
+        <b-collapse id="accordion-1" visible accordion="my-accordion" role="tabpanel">
+          <b-card-body>
+            <b-card-text>
+              
+              <b-list-group>
+                <b-list-group-item 
+                  v-bind:key="s['symbol']"
+                  v-for="s in strategy"
+                  class="flex-column align-items-start">
+                  <div class="d-flex w-100 justify-content-between">
+                    <h3 class="mb-2">{{ s['title'] }}</h3>
+                    <b-icon font-scale="1.5" icon="eye-slash" v-on:click="hideSymbol(s['symbol'])" />
+                  </div>
+                  <small><div class="text-left" v-html="s['content']"></div></small>
+                </b-list-group-item>
+              </b-list-group>
+              
+            </b-card-text>
+          </b-card-body>
+        </b-collapse>
+      </b-card>
 
-    <!--
-    <b-iconstack
-      font-scale="1.5"
-      class="position-absolute"
-      style="bottom: 1.0em; right: 1.0em;"
-      v-on:click="resetHiddenSymbol">
-      <b-icon icon="arrow-clockwise" scale="0.75" />
-      <b-icon icon="circle" />
-    </b-iconstack>
-    -->
-    
+      <!-- Scheme -->
+      <b-card no-body class="mb-1">
+        <b-card-header header-tag="header" class="p-1" role="tab">
+          <b-button block v-b-toggle.accordion-2 variant="primary">Scheme</b-button>
+        </b-card-header>
+        <b-collapse id="accordion-2" visible accordion="my-accordion" role="tabpanel">
+          <b-card-body>
+            <b-card-text>
+              
+              <b-list-group>
+                <b-list-group-item 
+                  v-bind:key="s['symbol']"
+                  v-for="s in scheme"
+                  class="flex-column align-items-start">
+                  <div class="d-flex w-100 justify-content-between">
+                    <h3 class="mb-2">{{ s['title'] }}</h3>
+                    <b-icon font-scale="1.5" icon="eye-slash" v-on:click="hideSymbol(s['symbol'])" />
+                  </div>
+                  <small><div class="text-left" v-html="s['content']"></div></small>
+                </b-list-group-item>
+              </b-list-group>
+              
+            </b-card-text>
+          </b-card-body>
+        </b-collapse>
+      </b-card>
+
+      <!-- Reset Button -->
+      <b-card no-body class="mb-1" v-if="hiddenSymbol.length > 0">
+        <b-card-header header-tag="header" class="p-1" role="tab">
+          <b-button block variant="secondary" v-on:click="resetHiddenSymbol">Reset hidden</b-button>
+        </b-card-header>
+      </b-card>
+
+      <!--
+      <b-iconstack
+        font-scale="1.5"
+        class="position-absolute"
+        style="bottom: 1.0em; right: 1.0em;"
+        v-on:click="resetHiddenSymbol">
+        <b-icon icon="arrow-clockwise" scale="0.75" />
+        <b-icon icon="circle" />
+      </b-iconstack>
+      -->
+      
+    </div>
+
   </div>
 </template>
 
 <script>
+const LEAGUE = {
+  Q1: 'Q1',
+  Q2: 'Q2',
+  Q3: 'Q3',
+  Q4: 'Q4'
+}
+
 export default {
   name: 'Strategy',
   data: function () {
     return {
       currentRuleset: 'GG1 Season 1',
+      selectedRuleset: 'GG1 Season 1',
+      selectedRulesetOptions: [
+        { value: 'GG1 Season 1', text: 'GG1 Season 1' },
+        { value: LEAGUE.Q1, text: '2021 The First Quater Malifaux League  Q1 (2021.03.12 - 2021.03.19)' },
+        { value: LEAGUE.Q2, text: '2021 The First Quater Malifaux League  Q2 (2021.03.19 - 2021.03.26)' },
+        { value: LEAGUE.Q3, text: '2021 The First Quater Malifaux League  Q3 (2021.03.26 - 2021.04.02)' },
+        { value: LEAGUE.Q4, text: '2021 The First Quater Malifaux League  Q4 (2021.04.02 - 2021.04.11)' }
+      ],
       ruleset: [],
       hiddenSymbol: []
     }
@@ -92,18 +115,42 @@ export default {
         return []
       }
 
-      return this.ruleset
-        .filter(r => r['ruleset'] === this.currentRuleset)[0]['strategy']
-        .filter(s => this.hiddenSymbol.indexOf(s['symbol']) < 0)
+      var ruleset = this.ruleset.filter(r => r['ruleset'] === this.currentRuleset)[0]
+      var strategy = ruleset['strategy']
+
+      switch (this.selectedRuleset) {
+        case LEAGUE.Q1:
+          return strategy.filter(s => s['symbol'] === 'Crow')
+        case LEAGUE.Q2:
+          return strategy.filter(s => s['symbol'] === 'Ram')
+        case LEAGUE.Q3:
+          return strategy.filter(s => s['symbol'] === 'Book')
+        case LEAGUE.Q4:
+          return strategy.filter(s => s['symbol'] === 'Mask')
+      }
+
+      return strategy.filter(s => this.hiddenSymbol.indexOf(s['symbol']) < 0)
     },
     scheme: function () {
       if (this.ruleset.length <= 0) {
         return []
       }
 
-      return this.ruleset
-        .filter(r => r['ruleset'] === this.currentRuleset)[0]['scheme']
-        .filter(s => this.hiddenSymbol.indexOf(s['symbol']) < 0)
+      var ruleset = this.ruleset.filter(r => r['ruleset'] === this.currentRuleset)[0]
+      var scheme = ruleset['scheme']
+
+      switch (this.selectedRuleset) {
+        case LEAGUE.Q1:
+          return scheme.filter(s => ['1', '6', '8', '10', '12'].indexOf(s['symbol']) >= 0)
+        case LEAGUE.Q2:
+          return scheme.filter(s => ['2', '3', '4', '5', '7'].indexOf(s['symbol']) >= 0)
+        case LEAGUE.Q3:
+          return scheme.filter(s => ['3', '5', '7', '9', '11'].indexOf(s['symbol']) >= 0)
+        case LEAGUE.Q4:
+          return scheme.filter(s => ['6', '8', '4', '12', '13'].indexOf(s['symbol']) >= 0)
+      }
+
+      return scheme.filter(s => this.hiddenSymbol.indexOf(s['symbol']) < 0)
     }
   },
   methods: {
